c675f2b7816acea5052fa97bde18ea7df4edaa5b
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-08 21:52:04 -0500

    initial commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Connie Liu on 7/8/18.
+//  Copyright © 2018 Connie Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
