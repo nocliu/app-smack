bfcb4b55e65a6324882b409953fc26170f2b967f
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-09 22:39:50 -0500

    ChannelVC gradient implemented, ChannelVC created

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Connie Liu on 7/9/18.
+//  Copyright © 2018 Connie Liu. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 1, green: 0.5374840546, blue: 0.08131992758, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 1, green: 0.4839150305, blue: 0.5987215288, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+}
