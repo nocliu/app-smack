68ab4ee2efcbbf4d4e5753e9818b6894eea23b0b
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-10 19:12:13 -0500

    Login interface built

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Connie Liu on 7/10/18.
+//  Copyright © 2018 Connie Liu. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Connie Liu on 7/10/18.
+//  Copyright © 2018 Connie Liu. All rights reserved.
+//
+
+import Foundation
+
+// segues
+let TO_LOGIN = "toLogin"
