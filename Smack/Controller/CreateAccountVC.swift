8aed7ceadc5ba7f120fc47b40a3353760f91a336
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-10 20:10:33 -0500

    Create account interface built

---
+++ Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,21 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by Connie Liu on 7/10/18.
+//  Copyright © 2018 Connie Liu. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND_TO_CHANNEL, sender: nil)
+    }
+    
+}
