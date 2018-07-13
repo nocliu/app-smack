fc0124bbac0084143165738449b965cdc3c94179
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-12 22:11:47 -0500

    Firebase stuff

--- Podfile
+++ Podfile
@@ -10,5 +10,16 @@ target 'Smack' do
   pod 'Alamofire'
   pod 'SwiftyJSON'
   pod 'Socket.IO-Client-Swift'
+  pod 'Firebase'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
   
+end
+
+post_install do |installer|
+    installer.pods_project.targets.each do |target|
+        target.build_configurations.each do |config|
+            config.build_settings['CLANG_WARN_DOCUMENTATION_COMMENTS'] = 'NO'
+        end
+    end
 end
--- Smack/AppDelegate.swift
+++ Smack/AppDelegate.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        let myDatabase = Database.database().reference()
+        myDatabase.setValue("It works!!")
+        
         return true
     }
 
