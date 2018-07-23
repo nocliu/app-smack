455850ea13348273b4ddde03a16083982ee96e30
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-22 22:03:22 -0500

    created web request

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://chattedup.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNEL = "unwindToChannel"
+
+// user defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
