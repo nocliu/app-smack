66bc57865de29ebdb1d0f625c6b615adb8ffa61b
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-22 22:50:46 -0500

    user registration working

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,8 +10,31 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func createAccountButtonPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let password = passwordTxt.text , passwordTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("registered user")
+            }
+        }
+    }
+    
+    @IBAction func chooseAvatarButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backgroundColorButtonPressed(_ sender: Any) {
     }
     
     @IBAction func closeButtonPressed(_ sender: Any) {
--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -46,7 +46,7 @@ class AuthService {
         
         let lowercaseEmail = email.lowercased()
         let header = [
-            "Content-Type" : "application/json; charset=utf-8"
+            "Content-Type": "application/json; charset=utf-8"
         ]
         let body: [String: Any] = [
             "email": lowercaseEmail,
