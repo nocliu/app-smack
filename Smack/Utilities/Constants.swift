4051542b623cff199985f162967c5c3ad80566a8
Connie Liu <connieliu@Lauras-MacBook-Pro-2.local>
2018-07-23 21:23:32 -0500

    used SwiftyJSON to log users in

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -13,6 +13,7 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 // URL Constants
 let BASE_URL = "https://chattedup.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 // segues
 let TO_LOGIN = "toLogin"
@@ -23,3 +24,9 @@ let UNWIND_TO_CHANNEL = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
+
